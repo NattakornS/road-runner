641ee668c12c2b5b5ecbb0a1571c3c965097fa2e
Nattakorn.Kingman@gmail.com <Nattakorn.Kingman@gmail.com@a20d5f39-75d3-3556-aeba-ffa180625963>
2014-01-05 16:57:34 +0000

    download dialog.

--- trunk/Roadrunner/src/com/senior/roadrunner/server/DialogConnect.java
+++ trunk/Roadrunner/src/com/senior/roadrunner/server/DialogConnect.java
@@ -10,18 +10,18 @@ public class DialogConnect extends ProgressDialog{
 	private AsyncTask task;
 	
 	@SuppressWarnings("rawtypes")
-	public DialogConnect(Context context,AsyncTask task) {
+	public DialogConnect(Context context) {//,AsyncTask task
 		//à¸ªà¸£à¹?à¸²à¸? Dialog à¸?à¸²à¸? super class
 		super(context);
 		
 		//à¹€à¸?à¸·à¹?à¸­à¸¡ AsyncTask (ConnectServer) à¹€à¸?à¹?à¸²à¸?à¸±à¸? Dialog
-		this.task = task;
+//		this.task = task;
 	}
 
 	//à¸–à¹?à¸²à¸¡à¸µà¸?à¸²à¸£à¸¢à¸?à¹€à¸¥à¸´à¸?à¸•à¸­à¸?à¸—à¸µà¹? Dialog à¸?à¸¶à¹?à¸?à¸¡à¸²à¸?à¸°à¸¡à¸²à¸—à¸³à¸?à¸²à¸? Function à¸?à¸µà¹?
 	public void cancel() {
 		//à¸¢à¸?à¹€à¸¥à¸´à¸?à¹€à¸?à¸·à¹?à¸­à¸¡à¸•à¹?à¸­à¸?à¸±à¸? Server
-		task.cancel(true);
+//		task.cancel(true);
 		super.cancel();
 	}
 	
